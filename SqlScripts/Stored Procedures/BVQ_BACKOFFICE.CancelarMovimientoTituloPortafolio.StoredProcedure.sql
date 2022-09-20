c35e4abf0ff3fdca2fc91ab372d20175fd543ad4
emancero <emancero@gmail.com>
2022-09-20 16:04:48 -0500

    EMN: MEJ - Aumentar SPs que se optimizarán luego y que antes estaban en NewPortfolios.sql Aumentar esquema BVQ_BACKOFFICE para evitar errores.

---
+++ SqlScripts/Stored Procedures/BVQ_BACKOFFICE.CancelarMovimientoTituloPortafolio.StoredProcedure.sql
@@ -0,0 +1,46 @@
+-- ==========================================================================================
+-- Author:		Patricio Villacis G
+-- Create date: 08-02-2018
+-- Description:	Cancela el movimiento de un título portafolio
+-- Parametros:  
+-- Cambios:		
+-- ==========================================================================================
+
+
+CREATE PROCEDURE [BVQ_BACKOFFICE].[CancelarMovimientoTituloPortafolio]
+	 @i_htp_id int
+	,@i_lga_id int		
+AS
+BEGIN
+
+	SET NOCOUNT ON;	
+
+	DECLARE @v_id_estado int;
+	
+	EXEC @v_id_estado = [BVQ_ADMINISTRACION].ObtenerIdEstadoCatalogo
+	@i_code = N'BCK_ES_TIT_POR',
+	@i_status = N'E';
+
+	EXEC	[BVQ_SEGURIDAD].[RegistrarAuditoria]
+		@i_lga_id = @i_lga_id,
+		@i_tabla = N'HISTORICO_TITULOS_PORTAFOLIO',
+		@i_esquema = N'BVQ_BACKOFFICE',
+		@i_operacion = N'U',
+		@i_subTipo = N'A',
+		@i_columIdName = N'HTP_ID',
+		@i_idAfectado = @i_htp_id;
+		
+	UPDATE BVQ_BACKOFFICE.HISTORICO_TITULOS_PORTAFOLIO
+	SET --HTP_FECHA_OPERACION = BVQ_ADMINISTRACION.ObtenerFechaSistema(),
+		HTP_ESTADO = @v_id_estado
+	WHERE HTP_ID = @i_htp_id
+
+    EXEC	[BVQ_SEGURIDAD].[RegistrarAuditoria]
+		@i_lga_id = @i_lga_id,
+		@i_tabla = N'HISTORICO_TITULOS_PORTAFOLIO',
+		@i_esquema = N'BVQ_BACKOFFICE',
+		@i_operacion = N'U',
+		@i_subTipo = N'N',
+		@i_columIdName = N'HTP_ID',
+		@i_idAfectado = @i_htp_id;
+END
