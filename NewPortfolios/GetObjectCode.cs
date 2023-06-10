8d3de4ca49aefa783aef2417085da43c36c74c45
emanc <emanc@DESKTOP-3PHFD5V>
2023-06-10 11:52:44 -0500

    En GetCode separar StoredProcedure enn Stored Procedure para buscar el folder

--- NewPortfolios/GetObjectCode.cs
+++ NewPortfolios/GetObjectCode.cs
@@ -5,8 +5,9 @@ class GetObjectCode
     public string FilesRoot { get; set; } = @"..\..\..\..\SqlScripts\";
     public string GetCode(string fullName, string type)
     {
+        string folderName = (type == "StoredProcedure") ? "Stored Procedure" : type;
         return File.ReadAllText(FilesRoot
-            + type+@"s\"
+            + folderName+@"s\"
             + fullName+@"."
             + type+@".sql");
     }
