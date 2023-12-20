110b9ca26f38603fcf53beefaf6ed5aa1430406f
emanc <emanc@DESKTOP-3PHFD5V>
2023-12-20 14:18:01 -0500

    EMN: REQ - Campos de provisión en ObtenerDetallePortafolioConLiquidez

--- SqlScripts/Stored Procedures/ObtenerDetallePortafolioConLiquidez.sql
+++ SqlScripts/Stored Procedures/ObtenerDetallePortafolioConLiquidez.sql
@@ -1,4 +1,4 @@
-﻿create  procedure bvq_backoffice.ObtenerDetallePortafolioConLiquidez(
+﻿create procedure bvq_backoffice.ObtenerDetallePortafolioConLiquidez(
 	 @i_idPortfolio		int				--Identificador del portafolio
 	,@i_fechaIni		datetime
 	,@i_fechaFin		datetime
@@ -48,11 +48,12 @@ begin
 	,TIV_NUMERO_EMISION_SEB
 	,TIV_FRECUENCIA
 	,IPR_ES_CXC
+	,fecha_original
 	)
 	select --* into bvq_backoffice.evtTemp
-	oper,htp_id,es_vencimiento_interes,fecha,montoOper,vep_valor_efectivo,en_liquidez,por_id,saldo_liquidez,voucher_exists,lip_cliente_id,htp_tpo_id,htp_fecha_operacion,tasa_cupon,porv_retencion,iAmortizacion,nombre,por_codigo,liquidez_descripcion,ems_nombre
- 
-,grc_codigo,tvl_codigo,tiv_fecha_vencimiento,tiv_tipo_valor,tpo_numeracion,vep_id,vep_cta_id,vep_other_account,amount,account,vep_renovacion,ttl_id,vep_observaciones,ttl_nombre,lip_retencion,com_id,lip_documento,cliente_nombre,htp_numeracion_clean,fecha_compra,por_tipo,tpo_categoria,vep_fecha,com_numero_comprobante,en_espera,evp_id,liq_compra
+	 oper,htp_id,es_vencimiento_interes,fecha,montoOper,vep_valor_efectivo,en_liquidez,por_id,saldo_liquidez,voucher_exists,lip_cliente_id,htp_tpo_id,htp_fecha_operacion,tasa_cupon,porv_retencion,iAmortizacion,nombre,por_codigo,liquidez_descripcion,ems_nombre
+
+	,grc_codigo,tvl_codigo,tiv_fecha_vencimiento,tiv_tipo_valor,tpo_numeracion,vep_id,vep_cta_id,vep_other_account,amount,account,vep_renovacion,ttl_id,vep_observaciones,ttl_nombre,lip_retencion,com_id,lip_documento,cliente_nombre,htp_numeracion_clean,fecha_compra,por_tipo,tpo_categoria,vep_fecha,com_numero_comprobante,en_espera,evp_id,liq_compra
  
 	--POR_PUBLIC_2
 	,por_public
@@ -69,6 +70,7 @@ begin
 	,TIV_NUMERO_EMISION_SEB
 	,TIV_FRECUENCIA
 	,IPR_ES_CXC
+	,fecha_original
 	from bvq_backoffice.ObtenerDetallePortafolioConLiquidezView
 	--where @i_idPortfolio=-1 or es_vencimiento_interes=0
  
@@ -84,7 +86,13 @@ begin
 	,@prevPorId=por_id
 	from bvq_backoffice.evtTemp with(tablockx)
 	where (abs(round(amount,2))>0.05e or oper=2)
+
 	select *,TPO_REESTRUCTURACION=CASE WHEN TPO_NUMERACION='2014-4933' THEN 1 ELSE 0 END
+	,(iAmortizacion+amount) AS 'total_cuota'
+	,diasTran=dbo.fnDiasEu(case when fecha_compra>TFL_FECHA_INICIO then fecha_compra else tfl_fecha_inicio end,dateadd(d,-day(fecha),fecha),354)
+	,originalProvision	=dbo.fnDiasEu(case when fecha_compra>TFL_FECHA_INICIO then fecha_compra else tfl_fecha_inicio end,dateadd(d,-day(fecha),fecha),354)/dias_cupon * iamortizacion
+	,provision			=dbo.fnDiasEu(case when fecha_compra>TFL_FECHA_INICIO then fecha_compra else tfl_fecha_inicio end,dateadd(d,-day(fecha),fecha),354)/dias_cupon * iamortizacion
+						+isnull(evp_ajuste_provision,0)
 	from bvq_backoffice.evtTemp where fecha between @i_fechaIni and @i_fechaFin
 	and (@i_client_id=lip_cliente_id or @i_client_id is null)
 	and (@i_idPortfolio=por_id or @i_idPortfolio=-1)
