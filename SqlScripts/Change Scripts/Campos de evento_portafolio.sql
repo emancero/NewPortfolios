fec077561e7a650eecf8fab61c5016a7636f4653
Eduardo Mancero <emancero@gmail.com>
2024-02-09 14:10:28 -0500

    EMN: REQ - Campo evp_referencia

---
+++ SqlScripts/Change Scripts/Campos de evento_portafolio.sql
@@ -0,0 +1,6 @@
+﻿IF NOT EXISTS(
+	select * from sys.columns
+	where object_id=object_id('BVQ_BACKOFFICE.LIQUIDEZ_CACHE')
+	and name='evp_valor_efectivo'
+)
+	alter table BVQ_BACKOFFICE.EVP_VALOR_EFECTIVO ADD evp_valor_efectivo float
