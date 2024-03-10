b9a03d5a6c6634b15335d51ab6778ce910126f12
Eduardo Mancero <emancero@gmail.com>
2024-03-10 13:49:07 -0500

    EMN: MEJ - Borrar comentarios y simplficar cálculo de depósito total en LiqIntProv

--- SqlScripts/Views/LiqIntProv.sql
+++ SqlScripts/Views/LiqIntProv.sql
@@ -2,9 +2,6 @@
 	with LiqProp as
 	(
 		select
-		 --fechaIni=max(s.fecha)
-		 --,dias_cupon=max(s.dias_cupon)--fechaIni=s.fecha-day(s.fecha)
-		 --,prop=convert(float,day(max(s.fecha)))/max(s.dias_cupon)
 		saldo=sum(monto)
 		,htp_compra=max(case when r=1 then htp_compra end)
 		,hist_fecha_compra=max(case when r=1 then hist_fecha_operacion end)
@@ -15,10 +12,8 @@
 		,TPO_COMISION_BOLSA=max(case when r=1 then TPO_COMISION_BOLSA end)
 		,TPO_COMISIONES=max(case when r=1 then TPO_COMISIONES end)
 		,tpo_id=max(tpo_id)
-		--,iamortizacion=max(iamortizacion)
 		,por_ord
 		,plazo=max(plazo)
-		--,tpo_precio_ingreso
 		,TPO_PRECIO_EFECTIVO=max(TPO_PRECIO_EFECTIVO)
 		,itrans=sum(itrans)
 		,evp_referencia=max(evp_referencia)
@@ -61,32 +56,12 @@
 	--vep_valor_efectivo,
 	amount0=amount,
 	amountCosto=
-	/*round(amount,2)
-	-round(
-		(1-e.hist_precio_compra/100.0)*htp_compra/htp_compra*(-montooper)
-	,2)
-	+round(
-		comisiones/htp_compra*(-montooper)
-	,2)*/
 	round(
 		coalesce(
 			EVP_AJUSTE_VALOR_EFECTIVO
 			,prEfectivo
-			*coalesce(capMonto,(-montooper))
+			*coalesce(capMonto,-montooper)
 		)
-		/*isnull(prEfectivo,0)
-		*coalesce(capMonto,(-montooper),0)
-		+isnull(EVP_AJUSTE_VALOR_EFECTIVO,0)*/
-		/*coalesce(
-			e.evp_pago_efectivo
-			,
-			case when hist_fecha_compra>='20230601' then
-				(e.hist_precio_compra/100.0*htp_compra+comisiones)
-				/htp_compra*coalesce(capMonto,(-montooper))
-			else
-				e.hist_precio_compra/100.0*coalesce(capMonto,(-montooper))
-			end
-		)*/
 	,2)
 	--o valefe+comisiones
 
@@ -104,69 +79,43 @@
 					case when tvl_codigo in ('PCO','FAC') and tasa_cupon=0 then
 						hist_precio_compra/100.0 * htp_compra
 					else
-						coalesce(capMonto,(-montooper))
+						coalesce(capMonto,-montooper)
 					end
+				--fin depósito total capital+interes
 
 				-
 				round(
 					coalesce(
 						EVP_AJUSTE_VALOR_EFECTIVO
 						,prEfectivo
-						*coalesce(capMonto,(-montooper))
+						*coalesce(capMonto,-montooper)
 					)
-
-					/*isnull(prEfectivo,0)
-					*coalesce(capMonto,(-montooper),0)
-					+isnull(EVP_AJUSTE_VALOR_EFECTIVO,0)*/
 				,2)
 				-
 				isnull(UFO_USO_FONDOS,0)
-				/*coalesce(
-					e.evp_pago_efectivo
-					,case when hist_fecha_compra>='20230601' then
-						(e.hist_precio_compra/100.0*htp_compra+comisiones)/htp_compra*coalesce(capMonto,(-montooper))
-					else
-						e.hist_precio_compra/100.0*coalesce(capMonto,(-montooper))
-					end
-				)*/
-
-				/*-(e.hist_precio_compra/100.0*htp_compra+comisiones)/htp_compra*(-montooper)*/
-				-(pr/*+isnull(EVP_AJUSTE_PROVISION,0)*/)
+				-pr
 				-case when tpo_fecha_ingreso>TFL_FECHA_INICIO then ISNULL(itrans,0) else 0 end
 			end
-		/*else
-			diasInteres/e.dias_cupon*descAm
-		end*/
-
-	--,2)
  
-	,prov=pr/*case when tvl_codigo not in ('PCO','FAC') then
-		pr
-	else
-		(dias_cupon-diasInteres)/e.dias_cupon*descAm		
-	end*/-- +isnull(EVP_AJUSTE_PROVISION,0)
+	,prov=pr
 	,fechaIni=fecha,prop=convert(float,day(e.fecha))/e.dias_cupon
 	,e.*
 	,prov2=orgIAmortizacion-pr
 	from
 	(
 		select
-		 pr=s.provision--case when ipr_es_cxc=1 or tvl_codigo='BE' then 0 else diasTran/dias_cupon * iamortizacion end
+		 pr=s.provision
 		,orgIAmortizacion=iamortizacion+descAm
 		,*
 		from(
 			select e.*
-			--,fini=case when hist_fecha_compra>TFL_FECHA_INICIO then hist_fecha_compra else tfl_fecha_inicio end
 			,diasTran=dbo.fnDias(case when hist_fecha_compra>TFL_FECHA_INICIO then hist_fecha_compra else tfl_fecha_inicio end,dateadd(d,-day(e.fecha),e.fecha),355)
 			,diasInteres=case when tvl_codigo not in ('PCO','FAC') then convert(float,day(e.fecha)) else dias_cupon-dbo.fnDiasEu(hist_fecha_compra,dateadd(d,-day(e.fecha),e.fecha), 354) end
 
 
 			                  
 			,descAm=-((1-l.hist_precio_compra/100.0)-case when /*e.es_vencimiento_interes*/0=0 then comisiones/htp_compra else 0 end)*montooper
-			--,pagoInteres=case when tvl_codigo not in ('PCO','FAC') then iamortizacion else 0 end
-			--,l.saldo
 			,l.htp_compra,l.hist_fecha_compra,l.hist_precio_compra
-			--,l.val_efe_compra
 			,l.comisiones,l.por_ord
 			,precio_efectivo=coalesce(
 				l.TPO_PRECIO_EFECTIVO
@@ -174,7 +123,6 @@
 			)
 			,tiv.tiv_tipo_renta
 			,l.plazo
-			--,finMes=dateadd(d,-day(e.fecha),e.fecha)
 			,capMonto
 			,l.plazo_anterior
 			from costo l
@@ -186,4 +134,3 @@
 		) s
 	) e
 	--where tpo_numeracion='MDF-2013-12-19' and fecha='20231219'
---select * from bvq_backoffice.liqintprov where tpo_numeracion='ASS-2023-10-20' and fecha='20231207'
