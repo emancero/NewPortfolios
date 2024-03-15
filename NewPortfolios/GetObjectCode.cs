aba32deb41d9fbd600516a870be0a492faf8c266
Eduardo Mancero <emancero@gmail.com>
2024-03-15 10:31:01 -0500

    EMN: MEJ - Plural opcional en GetCode

--- NewPortfolios/GetObjectCode.cs
+++ NewPortfolios/GetObjectCode.cs
@@ -3,11 +3,12 @@
 class GetObjectCode
 {
     public string FilesRoot { get; set; } = @"..\..\..\..\SqlScripts\";
-    public string GetCode(string fullName, string type, bool suffix=true)
+    public string GetCode(string fullName, string type, bool suffix=true, bool plural=true)
     {
         string folderName = (type == "StoredProcedure") ? "Stored Procedure" : type;
         return File.ReadAllText(FilesRoot
-            + folderName+@"s\"
+            + folderName+(plural?"s":"")
+            + @"\"
             + fullName
             + (suffix?@"."+type:"")+@".sql");
     }
--- NewPortfolios/Program.cs
+++ NewPortfolios/Program.cs
@@ -207,12 +207,12 @@ using (TransactionScope scope = new TransactionScope())
     //
     comm.CommandText = "dropifexists 'BVQ_BACKOFFICE.ObtenerConfAsientoRecuperacion'";
     comm.ExecuteNonQuery();
-    comm.CommandText = (new GetObjectCode()).GetCode("ObtenerConfAsientoRecuperacion", "siisspolweb", suffix: false);
+    comm.CommandText = (new GetObjectCode()).GetCode("ObtenerConfAsientoRecuperacion", "siisspolweb", suffix: false, plural: false);
     comm.ExecuteNonQuery();
 
     comm.CommandText = "dropifexists 'BVQ_BACKOFFICE.GenerarRecuperacionInversion'";
     comm.ExecuteNonQuery();
-    comm.CommandText = (new GetObjectCode()).GetCode("GenerarRecuperacionInversion", "siisspolweb", suffix: false);
+    comm.CommandText = (new GetObjectCode()).GetCode("GenerarRecuperacionInversion", "siisspolweb", suffix: false, plural:false);
     comm.ExecuteNonQuery();
 
 
