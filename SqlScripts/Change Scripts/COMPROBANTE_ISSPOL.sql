4030a106fe5bd9925c35f7ac22d44cc5a28c76d9
Eduardo Mancero <emancero@gmail.com>
2024-03-20 16:56:07 -0500

    EMN: MEJ - Aumentar campos de la tabla COMPROBANTE_ISSPOL al git

---
+++ SqlScripts/Change Scripts/COMPROBANTE_ISSPOL.sql
@@ -0,0 +1,20 @@
+﻿IF NOT EXISTS(
+	select * from sys.columns
+	where object_id=object_id('BVQ_BACKOFFICE.COMPROBANTE_ISSPOL')
+	and name='TFL_PERIODO'
+)
+	alter table BVQ_BACKOFFICE.COMPROBANTE_ISSPOL ADD TFL_PERIODO INT
+
+IF NOT EXISTS(
+	select * from sys.columns
+	where object_id=object_id('BVQ_BACKOFFICE.COMPROBANTE_ISSPOL')
+	and name='HTP_FECHA_OPERACION'
+)
+	alter table BVQ_BACKOFFICE.COMPROBANTE_ISSPOL ADD HTP_FECHA_OPERACION DATETIME
+
+IF NOT EXISTS(
+	select * from sys.columns
+	where object_id=object_id('BVQ_BACKOFFICE.COMPROBANTE_ISSPOL')
+	and name='deterioro'
+)
+	alter table BVQ_BACKOFFICE.COMPROBANTE_ISSPOL ADD deterioro BIT
