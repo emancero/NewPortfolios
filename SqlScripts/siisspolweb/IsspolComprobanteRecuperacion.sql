03c20ab6d10007af7ae9f724b291f241099b53da
Eduardo Mancero <emancero@gmail.com>
2024-04-04 23:48:30 -0500

    EMN: ISS - En IsspolComprobanteRecuperacion temporalmente excluir inversiones mal unidas

--- SqlScripts/siisspolweb/IsspolComprobanteRecuperacion.sql
+++ SqlScripts/siisspolweb/IsspolComprobanteRecuperacion.sql
@@ -58,12 +58,14 @@
 				join [siisspolweb].siisspolweb.[inversion].[titulo] t on it.id_titulo=t.id_titulo
 			on t.id_emisor=e.idemisor and
 			(
-					t.fecha_vencimiento=cis.tiv_fecha_vencimiento
-				and t.fecha_emision=cis.tiv_fecha_emision
-				or (tiv_tipo_renta=154 and cis.tippap<>'FI')
-				or (cis.tippap='FI' and i.fecha=cis.tiv_fecha_emision)
-			)
-			or cis.tpo_numeracion='SGE-2023-03-31' and i.id_inversion=175
+				(
+						t.fecha_vencimiento=cis.tiv_fecha_vencimiento
+					and t.fecha_emision=cis.tiv_fecha_emision
+					or (tiv_tipo_renta=154 and cis.tippap<>'FI')
+					or (cis.tippap='FI' and i.fecha=cis.tiv_fecha_emision)
+				)
+				or cis.tpo_numeracion='SGE-2023-03-31' and i.id_inversion=175
+			) and i.id_inversion not in (229,230,231,233,234,235)
 			--fin unión con la inversión
 
 			join [siisspolweb].siisspolweb.[inversion].[fondo_inversion] fi 
