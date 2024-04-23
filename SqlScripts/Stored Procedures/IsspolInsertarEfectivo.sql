6961ba82defbdb9979eb109d750182be9500e198
Eduardo Mancero <emancero@gmail.com>
2024-04-23 04:42:38 -0500

    EMN: MEJ - Subir IsspolInsertarEfectivo e IsspolInsertarFlujoCaja al Git.

---
+++ SqlScripts/Stored Procedures/IsspolInsertarEfectivo.sql
@@ -0,0 +1,76 @@
+﻿CREATE procedure BVQ_BACKOFFICE.IsspolInsertarEfectivo @i_fecha datetime, @i_creacion_usuario varchar(20), @cola int, @o_id_efectivo int out, @o_msj varchar(200) out as
+begin
+	declare @log varchar(max)
+	set @o_id_efectivo=(select id_efectivo from flujocaja.r_efectivo e where datediff(d,e.fecha,@i_fecha)=0)
+	set @log='InsertarEfectivo oidefectivo0 '+rtrim(@o_id_efectivo)
+	exec bvq_administracion.isspolenviolog @log
+
+	if @o_id_efectivo is not null
+		return 1
+	/*else
+	begin
+		set @o_msj = 'MSJ-INFO: No existe el flujo de caja para el '+convert(varchar,@i_fecha,105)
+		raiserror(@o_msj,16,0)
+		return -1
+	end*/
+
+	insert into flujocaja.r_efectivo(
+		fecha,usuario,total_banco,total_financiero,total_inversion,total_operativo,total_flujo,estado,creacion_usuario,creacion_fecha,creacion_equipo,modifica_usuario,modifica_fecha,modifica_equipo
+	)
+	select
+	 fecha=@i_fecha--fc.fecha
+	,usuario=@i_creacion_usuario
+	,total_banco=0
+	,total_financiero=0
+	,total_inversion=0
+	,total_operativo=0
+	,total_flujo=0
+	,estado=1
+	,creacion_usuario=@i_creacion_usuario
+	,creacion_fecha=getdate()
+	,creacion_equipo='192.168.2.225'
+	,modifica_usuario=@i_creacion_usuario
+	,modifica_fecha=getdate()
+	,modifica_equipo='192.168.2.225'
+	--model.fecha,model.usuario,model.total_banco,model.total_financiero,model.total_inversion,model.total_operativo,model.total_flujo
+	--,model.estado,model.creacion_usuario,model.creacion_fecha,model.creacion_equipo,model.modifica_usuario,model.modifica_fecha,model.modifica_equipo
+	--from bvq_backoffice.IsspolFlujoCajaAInsertar fc
+
+	--get identity, asigna null si no hay un registro con esa fecha, es decir si falló la inserción
+	set @o_id_efectivo=(select id_efectivo from flujocaja.r_efectivo e where datediff(s,e.fecha,@i_fecha)=0)
+	print 'oidefectivo '+rtrim(@o_id_efectivo)
+	if @o_id_efectivo is null
+	begin
+		set @o_msj = 'MSJ-INFO: No se pudo insertar el flujo de caja para la fecha '+convert(varchar,@i_fecha,13)
+		return -1
+	end
+	else
+	begin
+		exec bvq_administracion.EnviarMsj @cola,'InsertarEfectivo',@o_id_efectivo,2,'flujocaja.efectivo'
+	end
+	--select top 10 * from siisspolweb.siisspolweb.flujocaja.efectivo order by id_efectivo desc
+	if not exists(select * from inversion.r_fondo_inversion)
+		raiserror ('MSJ-ERROR: No existen registros en la tabla fondo_inversion o servidor remoto incorrecto',16,0)
+
+	--saldos iniciales
+	insert into flujocaja.r_caja(
+		secuencial,id_efectivo,id_cuenta_banco,id_categoria_rubro,operacion,valor,valor_acumulado,acumula,referencia,creacion_usuario,creacion_fecha,creacion_equipo,modifica_usuario,modifica_fecha,modifica_equipo
+	)
+	select
+		 secuencial=0--row_number() over (partition by e_id_efectivo,fi.id_cuenta order by ifi.montoInversion)
+		,id_efectivo=@o_id_efectivo
+		,id_cuenta_banco=fi.id_cuenta
+		,id_categoria_rubro=120 --id_categoria_rubro:120 es id_rubro:64:'Saldo inicial'
+		,operacion='+'
+		,valor=0--sum(ifi.montoInversion)
+		,valor_acumulado=null
+		,acumula=0
+		,referencia='Saldo inicial'
+		,creacion_usuario=@i_creacion_usuario
+		,creacion_fecha=getdate()
+		,creacion_equipo='192.168.2.225'
+		,modifica_usuario=@i_creacion_usuario
+		,modifica_fecha=getdate()
+		,modifica_equipo='192.168.2.225'
+	from inversion.r_fondo_inversion fi
+end
---
+++ SqlScripts/Stored Procedures/IsspolInsertarFlujoCaja.sql
@@ -0,0 +1,75 @@
+﻿CREATE procedure BVQ_BACKOFFICE.IsspolInsertarFlujoCaja @i_numeracion varchar(200), @i_fecha datetime, @i_creacion_usuario varchar(20), @i_cola_id int, @i_lga_id int  as
+begin
+	exec BVQ_BACKOFFICE.IsspolInsertarCaja @i_numeracion, @i_fecha, @i_creacion_usuario
+
+	--log
+	insert into BVQ_BACKOFFICE.ISSPOL_LOG_FLUJO_EFECTIVO(
+		ILFE_ID_EFECTIVO,
+		ILFE_FECHA,
+		ILFE_TOTAL_INVERSION,
+		ILFE_TOTAL_FLUJO,
+		ILFE_TOTAL_FINANCIERO,
+		ILFE_TOTAL_BANCO,
+		ILFE_COLA_ID
+	)
+	select id_efectivo,getdate(),total_inversion,total_flujo,total_financiero,total_banco,@i_cola_id
+	from flujocaja.r_efectivo e
+	where datediff(d,e.fecha,@i_fecha)=0
+
+	--actualiza los totales en la tabla efectivo
+	update e set 
+	 total_inversion=-total
+	,total_flujo=-total
+	,total_financiero=total
+	,total_banco=total
+	from
+	flujocaja.efectivo e
+	join (
+		select e.id_efectivo,total=sum(valor)
+		from flujocaja.r_efectivo e--bvq_backoffice.FlujoCajaManualAActualizar e
+		join flujocaja.r_caja c on c.id_efectivo=e.id_efectivo
+		where referencia<>'Saldo inicial'
+		group by e.id_efectivo
+	) s on e.id_efectivo=s.id_efectivo
+	and datediff(d,e.fecha,@i_fecha)=0
+	
+	--log
+	insert into BVQ_BACKOFFICE.ISSPOL_LOG_FLUJO_CAJA(
+		ILFC_ID_EFECTIVO,
+		ILFC_ID_CAJA,
+		ILFC_FECHA,
+		ILFC_VALOR,
+		ILFC_USUARIO,
+		ILFC_OPERACION,
+		ILFC_REFERENCIA,
+		ILFC_COLA_ID
+	)
+	select
+	 c.id_efectivo
+	,id_caja
+	,c.creacion_fecha
+	,c.valor
+	,c.creacion_usuario
+	,c.operacion
+	,c.referencia
+	,@i_cola_id
+	from flujocaja.r_efectivo a
+	join flujocaja.r_caja c
+	on c.referencia='Saldo inicial' --solo actualiza los saldos iniciales
+	and c.id_efectivo=a.id_efectivo-- and c.id_cuenta_banco=a.id_cuenta_banco
+	where datediff(d,a.fecha,@i_fecha)=0
+	
+
+	--actualiza los totales en los registros iniciales de la tabla de caja
+	update s set
+	--select a.id_efectivo,a.id_cuenta_banco,
+	valor=totalInicial
+	from(
+		select c.valor,totalInicial=sum(valor) over (partition by e.id_efectivo,c.id_cuenta_banco)
+		from flujocaja.r_efectivo e
+		join flujocaja.r_caja c
+		on c.referencia='Saldo inicial' --solo actualiza los saldos iniciales
+		and c.id_efectivo=e.id_efectivo-- and c.id_cuenta_banco=e.id_cuenta_banco
+		where datediff(d,e.fecha,@i_fecha)=0
+	) s
+end
