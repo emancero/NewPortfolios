d50d3d2c068a0f1993864acca7918f8c59f18769
Eduardo Mancero <emancero@gmail.com>
2024-06-25 15:14:01 -0500

    EMN: ISS - En fnDias condiderar caso de fecha de inicio en el último día de febrero

--- SqlScripts/Functions/fnDias.sql
+++ SqlScripts/Functions/fnDias.sql
@@ -1,7 +1,12 @@
 ﻿create function dbo.fnDias(@StartDate DateTime, @EndDate DateTime, @base int) returns float as
 begin
+	declare @StartDay int=day(@StartDate)
 	if @base=354 and datediff(d,@StartDate,EOMONTH(@StartDate))=0
-		set @StartDate=dateadd(d,30-day(@StartDate),@StartDate)
+	begin
+		if @StartDay>30
+			set @StartDate=dateadd(d,30-day(@StartDate),@StartDate)
+		set @StartDay=30
+	end
 	if @base=354 and datediff(d,@EndDate,EOMONTH(@EndDate))=0
 		if day(@StartDate)<30
 			set @EndDate=dateadd(d,1,EOMONTH(@EndDate))
@@ -13,7 +18,7 @@ begin
 	when @base=354 then
 		datediff(m,@StartDate,@EndDate)*30
 		+ case when 1=0 and day(@endDate)=31 then 30 else day(@endDate) end
-		- case when 1=0 and day(@startDate)=31 then 30 else day(@startDate) end
+		- case when 1=0 and day(@startDate)=31 then 30 else @StartDay end
 	when @base in (355,356) THEN
 		datediff(d,@StartDate,@EndDate)
 	end
