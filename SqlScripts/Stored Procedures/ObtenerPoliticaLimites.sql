bde71a35db13f80cb06d2b4d1663ff77223d0d94
Eduardo Mancero <emancero@gmail.com>
2024-08-09 23:03:17 -0500

    EMN: REQ - Completar ObtenerPoliticaLimites

--- SqlScripts/Stored Procedures/ObtenerPoliticaLimites.sql
+++ SqlScripts/Stored Procedures/ObtenerPoliticaLimites.sql
@@ -1,6 +1,4 @@
-﻿exec dropifexists 'BVQ_BACKOFFICE.ObtenerPoliticaLimites'
-go
-create procedure BVQ_BACKOFFICE.ObtenerPoliticaLimites
+﻿create procedure BVQ_BACKOFFICE.ObtenerPoliticaLimites
 	 @i_fecha_corte datetime
 	,@i_lga_id int = null
 as
@@ -9,10 +7,13 @@ begin
 	insert into corteslist values (@i_fecha_corte,1)
 	exec bvq_backoffice.generarcompraventaflujo
 	;with a as(
-		select sal,TIPO_RENTA=tiv_tipo_renta
+		select tpo_recursos
+		,vn=sal*case when tiv_tipo_renta=154 then tiv_valor_nominal else 1 end
+		,TIPO_RENTA=tiv_tipo_renta
 		--,sector_general
-		,SECTOR=case when sector_general like 'SEC_PUB_%' then 'ESTADO'
-		when sector_general like '%_NFIN' then 'FINANCIERO'
+		,SECTOR=
+		case when sector_general like 'SEC[_]PUB[_]%' then 'ESTADO'
+		when sector_general like '%[_]FIN' then 'FINANCIERO'
 		when tvl_codigo in ('OBL','PCO') then 'OBLIGACIONES Y PAPEL COMERCIAL'
 		when tvl_codigo in ('REP') then 'REPORTOS'
 		when tvl_codigo in ('VCC') then 'TITULARIZACIONES'
@@ -20,21 +21,25 @@ begin
 		when tvl_codigo in ('ACC','ENC') then 'ACCIONES Y ENCARGO FID'
 		when tvl_codigo in ('FI') then 'FONDOS DE INVERSIÓN'
 		when tvl_codigo in ('CDP') then 'VALORES DE PARTICIPACIÓN'
-		end
+		end--,*
 		--when 
-		from bvq_backoffice.portafoliocorte pc
-	) select sal=sum(sal),TIPO_RENTA,sec.SECTOR,PCT from a
-	left join (values
-		 ('ESTADO',10,0.3)
-		,('FINANCIERO',20,0.25)
-		,('OBLIGACIONES Y PAPEL COMERCIAL',30,0.2)
-		,('REPORTOS',40,0.15)
-		,('TITULARIZACIONES',50,0.05)
-		,('FACTURAS COMERCIALES',60,0.05)
+		from bvq_backoffice.portafoliocorte pc where isnull(ipr_es_cxc,0)=0 and sal>0
+	) select sal=isnull(sum(vn),0),pai=isnull(sum(vn),0),sec.TIPO_RENTA,sec.SECTOR,PCT,alert
+	,r=row_number() over (partition by sec.TIPO_RENTA order by ord desc)
+	from
+	(values
+		 ('RENTA FIJA'		,'ESTADO'							,10,0.30,0.05)
+		,('RENTA FIJA'		,'FINANCIERO'						,20,0.25,0.05)
+		,('RENTA FIJA'		,'OBLIGACIONES Y PAPEL COMERCIAL'	,30,0.20,0.05)
+		,('RENTA FIJA'		,'REPORTOS'							,40,0.15,0.05)
+		,('RENTA FIJA'		,'TITULARIZACIONES'					,50,0.05,0.02)
+		,('RENTA FIJA'		,'FACTURAS COMERCIALES'				,60,0.05,0.02)
 
-		,('ACCIONES Y ENCARGO FID',70,0.3)
-		,('FONDOS DE INVERSIÓN',80,0.3)
-		,('VALORES DE PARTICIPACIÓN',90,0.4)
-	) sec(SECTOR,ord,PCT) on sec.SECTOR=a.sector
-	group by sec.sector,tipo_renta,pct,ord order by ord
+		,('RENTA VARIABLE'	,'ACCIONES Y ENCARGO FID'			,70,0.30,0.05)
+		,('RENTA VARIABLE'	,'FONDOS DE INVERSIÓN'				,80,0.30,0.05)
+		,('RENTA VARIABLE'	,'VALORES DE PARTICIPACIÓN'			,90,0.40,0.05)
+	) sec(TIPO_RENTA,SECTOR,ord,PCT,alert)
+	left join a
+	on sec.SECTOR=a.sector
+	group by sec.sector,sec.tipo_renta,pct,ord,alert order by ord
 end
