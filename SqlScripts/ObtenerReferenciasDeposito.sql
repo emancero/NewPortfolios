a22899b05ee444dee02ca30662b1e85c39ab8da6
Eduardo Mancero <emancero@gmail.com>
2024-09-27 08:13:46 -0500

    EMN: ISS - ObtenerReferenciasDeposito para validar que no se supere el valor del depósito.

---
+++ SqlScripts/ObtenerReferenciasDeposito.sql
@@ -0,0 +1,22 @@
+﻿create procedure BVQ_BACKOFFICE.ObtenerReferenciasDeposito
+	@i_id_masivas_transaccion int, @i_lga_id int=null
+as
+begin
+	select refValor=sum(lre.valor)
+	,valorDeposito=max(mt.valor)
+	,referencia=max(lre.referencia)
+	,titulos=dbo.stringagg(
+		lre.tpo_numeracion+':'+format(lre.valor,'c2','es-EC')
+		,', ')
+	--select lre.*,*
+	from BVQ_BACKOFFICE.Liquidez_Referencias_table lre
+	join siisspolweb.siisspolweb.banco.masivas_transaccion mt on mt.id_masivas_transaccion=lre.idMasivasTransaccion
+	WHERE
+	--tpo_numeracion=@i_tpo_numeracion AND tiv_id=@i_tiv_id AND datediff(hh,lre.fecha,@i_fecha)=0
+	--and
+	lre.idMasivasTransaccion=@i_id_masivas_transaccion
+	and lre_estado=1
+	and 1=1
+	group by lre.idMasivasTransaccion
+	--having sum(lre.valor)>max(mt.valor)
+end
