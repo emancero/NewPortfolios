faa8c3564229eafaf297c217c553b6c81d9030db
Eduardo Mancero <emancero@gmail.com>
2024-09-30 14:04:09 -0500

    EMN: ISS - InsertarHistoricoActivosInmobiliarios

---
+++ SqlScripts/Stored Procedures/InsertarHistoricoActivosInmobiliarios.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE BVQ_BACKOFFICE.InsertarHistoricoActivosInmobiliarios
+	@i_imb_id int, @i_hai_fecha date, @i_hai_valor float,@i_lga_id int=null
+as
+BEGIN
+	insert into [BVQ_BACKOFFICE].[HISTORICO_ACTIVOS_INMOBILIARIOS](
+	IMB_ID, HAI_FECHA, HAI_VALOR)
+	values (@i_imb_id, @i_hai_fecha, @i_hai_valor)
+END
