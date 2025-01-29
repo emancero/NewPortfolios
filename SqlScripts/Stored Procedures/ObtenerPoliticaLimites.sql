3ad0affd385c9c6785aa231b44e453cd59829ed3
Eduardo Mancero <emancero@gmail.com>
2025-01-28 21:38:49 -0500

    EMN: REQ - Cambios a reporte de política de límites. Tomar como base también las inversiones no privativas, y el mes anterior.

--- SqlScripts/Stored Procedures/ObtenerPoliticaLimites.sql
+++ SqlScripts/Stored Procedures/ObtenerPoliticaLimites.sql
@@ -5,9 +5,9 @@ as
 begin
 
 
-	declare @v_fecha_base datetime = EOMONTH('2024-06-28T10:09:40', -1)
+	declare @v_fecha_base datetime = EOMONTH(@i_fecha_corte,-1)--'2024-06-28T10:09:40', -1)
 	if exists(select * from bvq_administracion.parametro where par_codigo='POLITICA_MES' and par_valor='NO')
-		set @v_fecha_base=DATEADD(yy, DATEDIFF(yy, 0, getdate()), -1)
+		set @v_fecha_base=DATEADD(yy, DATEDIFF(yy, 0, @i_fecha_corte), -1)
 	
 	--exec bvq_backoffice.generarcompraventaflujo
 
@@ -16,11 +16,13 @@ begin
 	insert into [_temp].[ObtenerInfoPortfoliosPorFechaResult]
 	exec [BVQ_BACKOFFICE].[ObtenerInfoPortfoliosPorFecha] @v_fecha_base,null
 
-	select tipoRenta
+	
+	
+	select tipoRenta='nopriv'
 	,baseSal=
 		--caso excepcional: monto quemado el primer año de uso del sistema
 		--pues ciertos precios estaban incorrectos en los archivos manuales
-		case when datediff(d,@v_fecha_base,'20231231')=0 and tipoRenta='Renta fija' then
+		case when datediff(d,@v_fecha_base,'20231231')=0 then--and tipoRenta='Renta fija' then
 			505968256.55
 		--fin caso excepcional
 		else
@@ -28,13 +30,23 @@ begin
 				isnull(PRECIO_DE_HOY,1)+isnull(INTERES_GANADO,0))--,sum(valor_nominal)--isnull(sum(sal*case when tipo_renta='RENTA VARIABLE' then tiv_valor_nominal else 1 end),0)
 		end
 	,baseFecha=
-	formatmessage('INVERSIONES NO PRIVATIVAS RENTA %s (%s)'
-		,case when tipoRenta='Renta fija' then 'FIJA' when tipoRenta='Renta variable' then 'VARIABLE' end
+	formatmessage('INVERSIONES NO PRIVATIVAS (%s)'
+		--,case when tipoRenta='Renta fija' then 'FIJA' when tipoRenta='Renta variable' then 'VARIABLE' end
 		,format(@v_fecha_base,'dd/MM/yyyy'))
-	from (values('Renta variable'),('Renta fija')) v(tipoRenta)
-	left join [_temp].[ObtenerInfoPortfoliosPorFechaResult] pc on pc.tipo_renta=v.tipoRenta
+	from --(values('Renta variable'),('Renta fija')) v(tipoRenta)
+	--left join
+	[_temp].[ObtenerInfoPortfoliosPorFechaResult] pc --on pc.tipo_renta=v.tipoRenta
 	where isnull(ipr_es_cxc,0)=0 and VALOR_NOMINAL>0
-	group by tipoRenta order by tipoRenta
+	--group by tipoRenta
+	union
+	select
+	tipoRenta=null
+	,baseSal=sum(mov_saldo)
+	,baseFecha=formatmessage('INVERSIONES PRIVATIVAS (%s)',format(@v_fecha_base,'dd/MM/yyyy'))
+	from BVQ_BACKOFFICE.isspol_saldo_inicial s where datediff(MM,@v_fecha_base,mov_fecha)=1 and mov_cuenta_contable like '71303%'
+	order by tipoRenta
+
+	
 
 	delete from corteslist
 	insert into corteslist values (@i_fecha_corte,1)
@@ -55,7 +67,7 @@ begin
 		when tvl_codigo in ('VCC') then 'TITULARIZACIONES'
 		when tvl_codigo in ('FAC') then 'FACTURAS COMERCIALES'
 		when tvl_codigo in ('ACC','ENC') then 'ACCIONES Y ENCARGO FID'
-		when tvl_codigo in ('FI') then 'FONDOS DE INVERSIÓN'
+		when tvl_codigo in ('FI') then 'FONDOS DE INVERSIÓN COLECTIVO / COTIZADO'
 		when tvl_codigo in ('CDP','VTP') then 'VALORES DE PARTICIPACIÓN'
 		end--,*
 		--when 
@@ -72,13 +84,13 @@ begin
 		 ('RENTA FIJA'		,'ESTADO'							, 10, 0.30, 0.05)
 		,('RENTA FIJA'		,'FINANCIERO'						, 20, 0.25, 0.05)
 		,('RENTA FIJA'		,'OBLIGACIONES Y PAPEL COMERCIAL'	, 30, 0.20, 0.05)
-		,('RENTA FIJA'		,'REPORTOS'							, 40, 0.15, 0.05)
+		,('RENTA FIJA'		,'REPORTOS'							, 40, 0.03, 0.05)
 		,('RENTA FIJA'		,'TITULARIZACIONES'					, 50, 0.05, 0.02)
-		,('RENTA FIJA'		,'FACTURAS COMERCIALES'				, 60, 0.05, 0.02)
+		,('RENTA FIJA'		,'FACTURAS COMERCIALES'				, 60, 0.01, 0.02)
 
-		,('RENTA VARIABLE'	,'ACCIONES Y ENCARGO FID'			, 70, 0.30, 0.05)
-		,('RENTA VARIABLE'	,'FONDOS DE INVERSIÓN'				, 80, 0.30, 0.05)
-		,('RENTA VARIABLE'	,'VALORES DE PARTICIPACIÓN'			, 90, 0.40, 0.05)
+		,('RENTA VARIABLE'	,'ACCIONES Y ENCARGO FID'			, 70, 0.03, 0.05)
+		,('RENTA VARIABLE'	,'FONDOS DE INVERSIÓN COLECTIVO / COTIZADO'				, 80, 0.05, 0.05)
+		,('RENTA VARIABLE'	,'VALORES DE PARTICIPACIÓN'			, 90, 0.08, 0.05)
 	) sec(TIPO_RENTA,SECTOR,ord,PCT,alert)
 	left join a
 	on sec.SECTOR=a.sector
