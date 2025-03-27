118f3037f06e4a1a1bf1cea41e8d9ecc2bcc4f4b
Eduardo Mancero <emancero@gmail.com>
2025-03-27 08:24:29 -0500

    EMN: ISS - Histórico de activos inmobiliarios

--- SqlScripts/Stored Procedures/InsertarHistoricoActivosInmobiliarios.sql
+++ SqlScripts/Stored Procedures/InsertarHistoricoActivosInmobiliarios.sql
@@ -1,8 +1,18 @@
-﻿CREATE PROCEDURE BVQ_BACKOFFICE.InsertarHistoricoActivosInmobiliarios
-	@i_imb_id int, @i_hai_fecha date, @i_hai_valor float,@i_lga_id int=null
+﻿create PROCEDURE BVQ_BACKOFFICE.InsertarHistoricoActivosInmobiliarios
+	@i_imb_id int, @i_hai_fecha date, @i_hai_valor float, @i_lga_id int=null
 as
 BEGIN
 	insert into [BVQ_BACKOFFICE].[HISTORICO_ACTIVOS_INMOBILIARIOS](
-	IMB_ID, HAI_FECHA, HAI_VALOR)
-	values (@i_imb_id, @i_hai_fecha, @i_hai_valor)
+	IMB_ID, HAI_FECHA, HAI_VALOR)--, HAI_FECHA_AVALUO, HAI_VALOR_AVALUO)
+	values (@i_imb_id, @i_hai_fecha, @i_hai_valor)--, @i_hai_fecha_avaluo, @i_hai_valor_avaluo)
+
+	EXEC	[BVQ_SEGURIDAD].[RegistrarAuditoria]
+	@i_lga_id = @i_lga_id,
+	@i_tabla = 'HISTORICO_ACTIVOS_INMOBILIARIOS',
+	@i_esquema = N'BVQ_BACKOFFICE',
+	@i_operacion = N'I',
+	@i_subTipo = N'N',
+	@i_columIdName = 'HAI_ID',
+	@i_idAfectado = @@IDENTITY;
+
 END
---
+++ SqlScripts/Stored Procedures/ObtenerActivosInmobiliariosISSPOL.sql
@@ -0,0 +1,17 @@
+﻿create procedure BVQ_BACKOFFICE.InsertarAvaluoActivosInmobiliarios
+	@i_imb_id int, @i_aai_fecha datetime, @i_aai_valor float, @i_lga_id int
+as
+begin
+	insert into BVQ_BACKOFFICE.AVALUO_ACTIVOS_INMOBILIARIOS(
+		IMB_ID, AAI_FECHA, AAI_VALOR
+	) values (@i_imb_id, @i_aai_fecha, @i_aai_valor)
+
+	EXEC	[BVQ_SEGURIDAD].[RegistrarAuditoria]
+	@i_lga_id = @i_lga_id,
+	@i_tabla = 'AVALUO_ACTIVOS_INMOBILIARIOS',
+	@i_esquema = N'BVQ_BACKOFFICE',
+	@i_operacion = N'I',
+	@i_subTipo = N'N',
+	@i_columIdName = 'AAI_ID',
+	@i_idAfectado = @@IDENTITY;
+end
