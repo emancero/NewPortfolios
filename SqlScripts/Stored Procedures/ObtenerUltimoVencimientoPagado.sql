43512e148c7ae806e132f48b82df53091ed702ee
Eduardo Mancero <emancero@gmail.com>
2025-05-15 08:31:09 -0500

    EMN: ISS - En ObtenerUltimoVencimientoPagado elegir la recuperación con mayor número de asientos.

--- SqlScripts/Stored Procedures/ObtenerUltimoVencimientoPagado.sql
+++ SqlScripts/Stored Procedures/ObtenerUltimoVencimientoPagado.sql
@@ -11,7 +11,13 @@ begin
 		where tiv_tipo_renta=154 and tpo_numeracion=@i_numeracion
 	)
 	begin
-		select * from bvq_backoffice.ObtenerDetallePortafolioConLiquidezView v
+		select distinct
+		 v.htp_id
+		,v.fecha_original
+		,v.es_vencimiento_interes
+		,v.amount
+		,v.htp_tpo_id
+		from bvq_backoffice.ObtenerDetallePortafolioConLiquidezView v
 		where --oper=0
 		fecha=htp_fecha_operacion
 		--and fecha=@i_fecha
@@ -30,7 +36,28 @@ begin
 		exec bvq_backoffice.prepararliquidezcache null
 	end
 
-	select * from bvq_backoffice.ObtenerDetallePortafolioConLiquidezView v
-	where fecha_original=@i_fecha
-	and tpo_numeracion=@i_numeracion
+
+	;with a as(
+		select htp_tpo_id,fecha_original,ipr_es_cxc,count(*) c
+		from bvq_backoffice.ObtenerDetallePortafolioConLiquidezView v
+		where TIV_TIPO_RENTA=153
+		and oper=1 and isnull(evp_abono,0)=0
+		group by htp_tpo_id,tpo_numeracion,fecha_original,ipr_es_cxc
+	), b as(
+		select r=row_number() over (partition by htp_tpo_id order by c, fecha_original desc),fecha_original,htp_tpo_id,c--fecha_original)
+		from a --where isnull(ipr_es_cxc,0)=0
+	), d as (
+		select distinct htp_tpo_id,fecha_original,c from b
+		where r=1
+	)
+	select distinct
+		 v.htp_id
+		,v.fecha_original
+		,v.es_vencimiento_interes
+		,v.amount
+		,v.htp_tpo_id
+	from bvq_backoffice.ObtenerDetallePortafolioConLiquidezView v
+	join d on v.htp_tpo_id=d.htp_tpo_id
+	where 1=1--fecha_original=@i_fecha
+	and v.tpo_numeracion=@i_numeracion
 end
