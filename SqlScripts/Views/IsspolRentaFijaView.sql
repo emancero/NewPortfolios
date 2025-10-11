b07500738b0216cc2566d0f8041a48e13aa5c118
Eduardo Mancero <emancero@gmail.com>
2025-10-11 09:25:06 -0500

    EMN: ISS - Issue con ATX en IsspolRentaFijaView. Preparación número 1 para entender siguiente cambio

--- SqlScripts/Views/IsspolRentaFijaView.sql
+++ SqlScripts/Views/IsspolRentaFijaView.sql
@@ -151,7 +151,9 @@ VALNOM_ANTERIOR=VALNOM_ANTERIOR,
    FECHA_VALOR_DE_COMPRA=fecha_compra,    
    VALOR_EFECTIVO_HISTORICO=
    case when min_tiene_valnom=1 or min_tiene_valnom=0 and httpo_id<1500 then
-	  isnull([TPO_INTERES_TRANSCURRIDO],0) + isnull([TPO_COMISION_BOLSA],0) + [htp_compra]*[htp_precio_compra]/case when [tiv_tipo_renta]=153 then 100e else 1e end
+	  isnull([TPO_INTERES_TRANSCURRIDO],0) + isnull([TPO_COMISION_BOLSA],0)
+      + [htp_compra]*[htp_precio_compra]
+      /case when [tiv_tipo_renta]=153 then 100e else 1e end
    end,    
    YIELD =
    CASE
