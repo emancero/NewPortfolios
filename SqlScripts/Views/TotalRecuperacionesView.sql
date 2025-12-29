dd87b8cfabb43f92e7a7f27a11a8ed0a4a1a590a
Eduardo Mancero <emancero@gmail.com>
2025-12-28 21:52:56 -0500

    EMN: ISS - Reordenar condiciones de where de TotalRecuperacionesView para que se parezcan más a las de ComprobateIsspol

--- SqlScripts/Views/TotalRecuperacionesView.sql
+++ SqlScripts/Views/TotalRecuperacionesView.sql
@@ -13,10 +13,11 @@
 		,Emisor=ems_nombre
 		,[Uso de fondos]=case when cir.rubro in ('intacc') then cir.UFO_USO_FONDOS end
 	FROM BVQ_BACKOFFICE.ComprobanteIsspolRubros cir
-	WHERE cir.rubro IN ('AMOUNT', 'amountcxc','INTAcc', 'PROV')
-	AND monto IS NOT NULL
+	WHERE
+	cir.tipo = 'C'
 	AND cir.acreedoraSinAux NOT LIKE '2%'
-	AND cir.tipo = 'C'
-	AND (cir.en_espera = 1 or cir.fecha<'20240301')
+	AND monto IS NOT NULL
     --AND left(prefijo,1) not in ('D','R')--
 	AND deterioro=0--(ipr_es_cxc = 1 or (ipr_es_cxc is null or ipr_es_cxc = 0 ) and deterioro = 0)
+	AND (cir.en_espera = 1 or cir.fecha<'20240301')
+	AND cir.rubro IN ('AMOUNT', 'amountcxc','INTAcc', 'PROV')
