68a0c8d78f202ec2526582138ae14a7fc9763c32
Eduardo Mancero <emancero@gmail.com>
2026-04-04 01:04:49 -0500

    EMN: REQ - Añadir menú SC Inversiones

--- NewPortfolios/Program.cs
+++ NewPortfolios/Program.cs
@@ -178,6 +178,8 @@ using (TransactionScope scope = new TransactionScope())
     //4-abr-2026
     comm.CommandText = (new GetObjectCode()).GetCode("Menu Estructuras SB", "Change Script", suffix: false);
     comm.ExecuteNonQuery();
+    comm.CommandText = (new GetObjectCode()).GetCode("Menu SC Inversiones", "Change Script", suffix: false);
+    comm.ExecuteNonQuery();
 
     //1-feb-2026
     /*comm.CommandText = (new GetObjectCode()).GetCode("HISTORICO_TITULOS_PORTAFOLIO", "Change Script", suffix: false);
---
+++ SqlScripts/Change Scripts/Menu SC Inversiones.sql
@@ -0,0 +1,63 @@
+﻿
+
+begin tran 
+
+DECLARE @UltimoId INT;
+
+SELECT @UltimoId = ISNULL(MAX(FUN_ID), 0)
+FROM bvq_seguridad.funcionalidad;
+
+IF NOT EXISTS (
+    SELECT 1 
+    FROM bvq_seguridad.funcionalidad 
+    WHERE FUN_NOMBRE = 'Inversiones SC'
+)
+BEGIN
+
+	INSERT INTO bvq_seguridad.funcionalidad
+	(
+		FUN_ID,
+		MOD_ID,
+		FUN_NOMBRE,
+		FUN_TIPO,
+		FUN_CODIGO,
+		FUN_ESTADO,
+		FUN_NOMBRE_DLL,
+		FUN_NOMBRE_CLASE,
+		FUN_PADRE,
+		FUN_AUDITAR,
+		FUN_ORDEN,
+		FUN_ES_VERSION2
+	)
+	VALUES
+	(
+		@UltimoId + 1,
+		4,
+		'Inversiones SC',
+		89,
+		'BCK_INVESTMENTS_ISSPOL',
+		82,
+		'Bvq.Sipla.Isspol.Module.dll',
+		'Bvq.Sipla.Isspol.Module.SCInvestmentsView',
+		21,
+		1,
+		106,
+		NULL
+	);
+
+	SELECT @UltimoId = ISNULL(MAX(FUN_ID), 0)
+	FROM bvq_seguridad.funcionalidad;
+ 
+	INSERT INTO bvq_seguridad.perfil_funcionalidad
+	(
+		PRF_ID,
+		FUN_ID
+	)
+	VALUES
+	(
+		1,
+		@UltimoId
+	);
+END
+
+commit tran 
