87668aa11dcb0412ee1740d36b7d909cc52f228a
David Betancourt <ddavebet@gmail.com>
2026-04-16 07:23:16 -0500

    DBN: ISS - Agregar sp ObtenerFirmasPorFecha

---
+++ SqlScripts/Stored Procedures/BVQ_ADMINISTRACION.ObtenerFirmasPorFecha.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE BVQ_ADMINISTRACION.ObtenerFirmasPorFecha
+    @fecha DATE,
+	@i_lga_id int
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    WITH FirmasConRango AS (
+        SELECT 
+            ISNULL(
+                LEAD(FIR_FECHA_VINCULACION) 
+                OVER (PARTITION BY FIR_CARGO ORDER BY FIR_FECHA_VINCULACION ASC),
+                '99991231'
+            ) AS FIR_FECHA_HASTA,
+            *
+        FROM BVQ_ADMINISTRACION.FIRMA
+    )
+    SELECT *
+    FROM FirmasConRango
+    WHERE @fecha >= FIR_FECHA_VINCULACION
+      AND @fecha <  FIR_FECHA_HASTA;
+
+END
