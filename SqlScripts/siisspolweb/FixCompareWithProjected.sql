bb7f8a183c080a8568435b4da9adb0a80c27fc0a
Eduardo Mancero <emancero@gmail.com>
2026-05-03 22:11:44 -0500

    EMN: ISS - Aumentar FixCompareWithProjected a git

---
+++ SqlScripts/siisspolweb/FixCompareWithProjected.sql
@@ -0,0 +1,5 @@
+﻿--de script de comparación cuando sum(total_cuota) not grouped by evp_id<>sum(plus)
+update evp set evp_valor_efectivo=newVe from bvq_backoffice.evento_portafolio evp
+join (
+	values(3189,94494.937,188989.87),(3190,567.563,1135,13)
+) v(evpId, newVe, oldVe) on v.evpId=evp.evp_id
