904a8665586a6da1d4c7fe1a427b772c95febcc4
David Betancourt <ddavebet@gmail.com>
2026-05-27 17:01:03 -0500

    DBN: REQ - Actualizar ObtenerTasaInternaRetornoIsspol

--- SqlScripts/Stored Procedures/ObtenerTasaInternaRetornoIsspol.sql
+++ SqlScripts/Stored Procedures/ObtenerTasaInternaRetornoIsspol.sql
@@ -5,13 +5,33 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    SELECT
-        POR_CODIGO,
-        DATEDIFF(M, @i_fecha_corte, fecha_vencimiento)                  AS mes,
-        ISNULL(saldo, 0)                                                AS recuperacion_capital,
-        ISNULL(valor_pactado, 0)                                        AS recuperacion_interes,
-        ISNULL(saldo, 0) + ISNULL(valor_pactado, 0)                     AS recuperacion_total,
-        (ISNULL(saldo, 0) + ISNULL(valor_pactado, 0))
-            / POWER(1.08, DATEDIFF(M, @i_fecha_corte, fecha_vencimiento) / 12.0) AS valor_presente
-    FROM [BVQ_BACKOFFICE].[CREDITO_CARTERA_CUOTA];
+    SELECT * FROM
+    (
+        SELECT
+            '' AS POR_CODIGO,
+            DATEDIFF(M, @i_fecha_corte, fecha_vencimiento) AS mes,
+            -(DATEDIFF(M, @i_fecha_corte, fecha_vencimiento) / 12.0) AS exponente,
+            ISNULL(SUM(saldo), 0) AS recuperacion_capital,
+            ISNULL(SUM(valor_pactado), 0) AS recuperacion_interes,
+            SUM(ISNULL(saldo, 0) + ISNULL(valor_pactado, 0)) AS recuperacion_total, -- COEFICIENTE
+            SUM((ISNULL(saldo, 0) + ISNULL(valor_pactado, 0)) / POWER(1.08, DATEDIFF(M, @i_fecha_corte, fecha_vencimiento) / 12.0)) AS valor_presente
+        FROM [BVQ_BACKOFFICE].[CREDITO_CARTERA_CUOTA]
+        GROUP BY DATEDIFF(M, @i_fecha_corte, fecha_vencimiento)
+
+        UNION
+
+        SELECT
+            '' AS POR_CODIGO,
+            DATEDIFF(M, @i_fecha_corte, CRC_FECHA_CIERRE) AS mes,
+            1 AS exponente,
+            NULL AS recuperacion_capital,
+            NULL AS recuperacion_interes,
+            -SUM(CRC_SALDO_PRESTAMO) AS recuperacion_total,
+            -SUM(CRC_SALDO_PRESTAMO) AS valor_presente
+        FROM BVQ_BACKOFFICE.CREDITOS_CARTERA
+        WHERE DATEDIFF(M, @i_fecha_corte, CRC_FECHA_CIERRE) = 0
+        GROUP BY CRC_FECHA_CIERRE
+    ) AS t
+    ORDER BY mes
 END
+GO
