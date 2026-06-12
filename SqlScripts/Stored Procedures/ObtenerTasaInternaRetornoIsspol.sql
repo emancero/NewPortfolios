52fd77bcaa25b53b536ec6e775a8d940c7be7614
David Betancourt <ddavebet@gmail.com>
2026-06-12 12:51:42 -0500

    DBN: REQ - Actualizar SP de Tasa Interna de Retorno

--- SqlScripts/Stored Procedures/ObtenerTasaInternaRetornoIsspol.sql
+++ SqlScripts/Stored Procedures/ObtenerTasaInternaRetornoIsspol.sql
@@ -1,76 +1,20 @@
 ﻿CREATE PROCEDURE [BVQ_BACKOFFICE].[ObtenerTasaInternaRetornoIsspol]
-    @i_fecha_corte DATE,
-    @i_lga_id      INT
+    @i_fecha_corte DATEtime,
+    @i_lga_id      INT=null
 AS
 BEGIN
-    SET NOCOUNT ON;
+--declare @i_fecha_corte date='20260430'
 
-	if (OBJECT_ID('[BVQ_BACKOFFICE].[CREDITO_CARTERA_CUOTA]') is NULL)
-	begin
-		select 
-				b.descripcion as por_codigo
-				,sum(valor) as total
-				,convert(date,cut.fecha_vencimiento) as fecha_vencimiento
-				,cr.id_cuenta
-				,max(convert(int,cut.pagada)) as pagada
-				,max(cut.id_rubro) as id_rubro
-				,sum(case when cut.id_rubro = 'K' then cut.saldo end) as saldo
-				,max(cr.id_estado) as id_estado
-				,sum(case when cut.id_rubro='I' then cut.valor_pactado end) as valor_pactado
-		into [BVQ_BACKOFFICE].[CREDITO_CARTERA_CUOTA]
-		from	siisspolweb.siisspolweb.credito.cuota cut
-				join siisspolweb.siisspolweb.credito.credito cr on cr.id_credito=cut.id_credito
-				join siisspolweb.siisspolweb.banco.cuenta b on b.id_cuenta=cr.id_cuenta
-		where cut.fecha_vencimiento>@i_fecha_corte
-		group by b.descripcion,cut.fecha_vencimiento, cr.id_cuenta
-	End
-	else 
-	begin
-		truncate table [BVQ_BACKOFFICE].[CREDITO_CARTERA_CUOTA]
-		insert into [BVQ_BACKOFFICE].[CREDITO_CARTERA_CUOTA]
-		select 
-				b.descripcion as por_codigo
-				,sum(valor) as total
-				,convert(date,cut.fecha_vencimiento) as fecha_vencimiento
-				,cr.id_cuenta
-				,max(convert(int,cut.pagada)) as pagada
-				,max(cut.id_rubro) as id_rubro
-				,sum(case when cut.id_rubro = 'K' then cut.saldo end) as saldo
-				,max(cr.id_estado) as id_estado
-				,sum(case when cut.id_rubro='I' then cut.valor_pactado end) as valor_pactado
-		from	siisspolweb.siisspolweb.credito.cuota cut
-				join siisspolweb.siisspolweb.credito.credito cr on cr.id_credito=cut.id_credito
-				join siisspolweb.siisspolweb.banco.cuenta b on b.id_cuenta=cr.id_cuenta
-		where cut.fecha_vencimiento>@i_fecha_corte
-		group by b.descripcion,cut.fecha_vencimiento, cr.id_cuenta
-	end
-
-    SELECT * FROM
-    (
-        SELECT
-            '' AS POR_CODIGO,
-            DATEDIFF(M, @i_fecha_corte, fecha_vencimiento) AS mes,
-            DATEDIFF(M, @i_fecha_corte, fecha_vencimiento) / 12.0 AS exponente,
-            ISNULL(SUM(saldo), 0) AS recuperacion_capital,
-            ISNULL(SUM(valor_pactado), 0) AS recuperacion_interes,
-            SUM(ISNULL(saldo, 0) + ISNULL(valor_pactado, 0)) AS recuperacion_total,
-            SUM((ISNULL(saldo, 0) + ISNULL(valor_pactado, 0)) / POWER(1.08, DATEDIFF(M, @i_fecha_corte, fecha_vencimiento) / 12.0)) AS valor_presente
-        FROM [BVQ_BACKOFFICE].[CREDITO_CARTERA_CUOTA]
-        GROUP BY DATEDIFF(M, @i_fecha_corte, fecha_vencimiento)
-
-        UNION
-
-        SELECT
-            '' AS POR_CODIGO,
-            DATEDIFF(M, @i_fecha_corte, CRC_FECHA_CIERRE) AS mes,
-            1 AS exponente,
-            NULL AS recuperacion_capital,
-            NULL AS recuperacion_interes,
-            -SUM(CRC_SALDO_PRESTAMO) AS recuperacion_total,
-            -SUM(CRC_SALDO_PRESTAMO) AS valor_presente
-        FROM BVQ_BACKOFFICE.CREDITOS_CARTERA
-        WHERE DATEDIFF(M, @i_fecha_corte, CRC_FECHA_CIERRE) = 0
-        GROUP BY CRC_FECHA_CIERRE
-    ) AS t
-    ORDER BY mes
+    SELECT mes
+    ,exponente
+    ,recuperacion_capital=sum(recuperacion_capital)
+    ,recuperacion_interes=sum(recuperacion_interes)
+    ,recuperacion_total=sum(recuperacion_total)
+    ,valor_presente=sum(valor_presente)
+    ,tasa
+    ,plazo
+    from bvq_backoffice.CreditoCuota
+    GROUP BY mes,exponente,tasa,plazo
+    ORDER BY tasa,plazo,
+    mes
 END
---
+++ SqlScripts/Views/CreditoCuota.sql
@@ -0,0 +1,15 @@
+﻿create view [BVQ_BACKOFFICE].[CreditoCuota] as
+    SELECT
+        '' AS POR_CODIGO,
+        datediff(m,fecha_corte,cut.fecha_vencimiento) mes,
+        datediff(m,fecha_corte,cut.fecha_vencimiento) / 12.0 AS exponente,
+        saldo AS recuperacion_capital,
+        valor_pactado AS recuperacion_interes,
+        ISNULL(saldo, 0) + ISNULL(valor_pactado, 0) AS recuperacion_total,
+        0.0 AS valor_presente,
+        tasa,
+        plazo=datediff(yy,fecha_corte,fecha_vencimiento_credito)
+    --into #x
+    --select count(*)
+    FROM [BVQ_BACKOFFICE].[CREDITO_CARTERA_CUOTA_2] cut
+    where tasa is not null
