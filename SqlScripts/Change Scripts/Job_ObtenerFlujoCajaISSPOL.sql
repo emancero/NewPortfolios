e691b2b972552e3fe30dd500e5739034bfa57d27
David Betancourt <ddavebet@gmail.com>
2026-06-15 08:58:14 -0500

    DBN: FIX - Corregir comando sql de job de recarga de CREDITO_CARTERA_CUOTA

--- SqlScripts/Change Scripts/Job_ObtenerFlujoCajaISSPOL.sql
+++ SqlScripts/Change Scripts/Job_ObtenerFlujoCajaISSPOL.sql
@@ -9,16 +9,32 @@ DECLARE @dbName NVARCHAR(128) = N'SicavStage'; -- << cambiar aqui el nombre de l
 
 -- 1. Crear el Job
 EXEC msdb.dbo.sp_add_job
-    @job_name = N'Job_ObtenerFlujoCajaISSPOL';
+    @job_name = N'Job_RecargarCarteraCuotaISSPOL';
 
--- 2. Agregar el paso que ejecuta el SP
-DECLARE @cmd NVARCHAR(500) = N'EXEC [BVQ_BACKOFFICE].[ObtenerFlujoCajaISSPOL]
-                                    @i_fechaFin = CAST(GETDATE() AS DATE),
-                                    @i_lga_id   = NULL;';
+-- 2. Agregar el paso que ejecuta el comando
+DECLARE @cmd NVARCHAR(MAX) = N'
+TRUNCATE TABLE [BVQ_BACKOFFICE].[CREDITO_CARTERA_CUOTA];
+
+INSERT INTO [BVQ_BACKOFFICE].[CREDITO_CARTERA_CUOTA]
+SELECT
+     b.descripcion AS por_codigo
+    ,valor
+    ,cr.id_cuenta
+    ,CONVERT(INT, cut.pagada)      AS pagada
+    ,cut.id_rubro                  AS id_rubro
+    ,cr.tasa
+    ,id_rubro
+    ,abono
+    ,valor_pactado
+FROM  siisspolweb.siisspolweb.credito.cuota cut
+JOIN  siisspolweb.siisspolweb.credito.credito cr ON cr.id_credito = cut.id_credito
+JOIN  siisspolweb.siisspolweb.banco.cuenta    b  ON b.id_cuenta   = cr.id_cuenta
+WHERE cut.fecha_vencimiento > ''20260430'';
+';
 
 EXEC msdb.dbo.sp_add_jobstep
-    @job_name          = N'Job_ObtenerFlujoCajaISSPOL',
-    @step_name         = N'Ejecutar ObtenerFlujoCajaISSPOL',
+    @job_name          = N'Job_RecargarCarteraCuotaISSPOL',
+    @step_name         = N'Ejecutar RecargarCarteraCuotaISSPOL',
     @subsystem         = N'TSQL',
     @command           = @cmd,
     @database_name     = @dbName,
@@ -34,9 +50,9 @@ EXEC msdb.dbo.sp_add_schedule
 
 -- 4. Vincular el schedule al Job
 EXEC msdb.dbo.sp_attach_schedule
-    @job_name      = N'Job_ObtenerFlujoCajaISSPOL',
+    @job_name      = N'Job_RecargarCarteraCuotaISSPOL',
     @schedule_name = N'Diario_3AM';
 
 -- 5. Registrar el Job en el servidor local
 EXEC msdb.dbo.sp_add_jobserver
-    @job_name = N'Job_ObtenerFlujoCajaISSPOL';
+    @job_name = N'Job_RecargarCarteraCuotaISSPOL';
