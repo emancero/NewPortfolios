bbee80c43e9af833c2aa9190f132ffb24b393a74
Eduardo Mancero <emancero@gmail.com>
2026-06-20 01:10:25 -0500

    EMN: ISS - Campos de tasa, abono, id_credito e id_numero_cuota en tabla CREDITO_CARTERA_CUOTA

--- SqlScripts/Change Scripts/Tabla CREDITO_CARTERA_CUOTA.sql
+++ SqlScripts/Change Scripts/Tabla CREDITO_CARTERA_CUOTA.sql
@@ -1,4 +1,6 @@
 ﻿CREATE TABLE [BVQ_BACKOFFICE].[CREDITO_CARTERA_CUOTA](
+	[id_credito] [varchar](20) not NULL,
+	[id_numero_cuota] [int] not NULL,
 	[por_codigo] [varchar](50) NULL,
 	[total] [money] NULL,
 	[fecha_vencimiento] [date] NULL,
@@ -7,7 +9,9 @@
 	[id_rubro] [varchar](3) NULL,
 	[saldo] [money] NULL,
 	[estado] [char](1) NULL,
-	[valor_pactado] [money] NULL
+	[valor_pactado] [money] NULL,
+	[tasa] [float] NULL,
+	[abono] [money] NULL
 ) ON [PRIMARY]
 
 ALTER TABLE [BVQ_BACKOFFICE].[CREDITO_CARTERA_CUOTA] ADD  DEFAULT ((0)) FOR [pagada]
