272e6ddebdf5989d6a0d2ee5cc00a8679f221064
Eduardo Mancero <emancero@gmail.com>
2026-06-30 11:08:09 -0500

    EMN: ISS - Campos tasa, abono y fecha_pago en tabla CREDITO_CARTERA_CUOTA

--- SqlScripts/Stored Procedures/RecargarCarteraCuotaISSPOL.sql
+++ SqlScripts/Stored Procedures/RecargarCarteraCuotaISSPOL.sql
@@ -16,6 +16,9 @@ begin
 		, abono
 		, estado
 		, valor_pactado
+		, id_credito
+		, id_numero_cuota
+		, fecha_pago
 		)
     SELECT
          b.descripcion AS por_codigo
@@ -28,9 +31,12 @@ begin
         ,abono
         ,estado
         ,valor_pactado
+		,cut.id_credito
+		,cut.id_numero_cuota
+		,cut.fecha_pago
     FROM siisspolweb.siisspolweb.credito.cuota cut
     JOIN siisspolweb.siisspolweb.credito.credito cr ON cr.id_credito = cut.id_credito
     JOIN siisspolweb.siisspolweb.banco.cuenta b ON b.id_cuenta = cr.id_cuenta
-    WHERE cut.fecha_vencimiento > '20260430';
+    WHERE cut.fecha_vencimiento > '20251130';
 
 end
