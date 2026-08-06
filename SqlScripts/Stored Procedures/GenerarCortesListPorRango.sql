35b315a9f8c7f5b3df25259a2e8836e48d1e7bc3
Eduardo Mancero <emancero@gmail.com>
2026-08-06 16:24:07 -0500

    EMN: ISS - En GenerarCortesListPorRango tomar día anterior como día inicial.

--- SqlScripts/Stored Procedures/GenerarCortesListPorRango.sql
+++ SqlScripts/Stored Procedures/GenerarCortesListPorRango.sql
@@ -1,9 +1,9 @@
-﻿create procedure GenerarCortesListPorRango
+﻿CREATE procedure GenerarCortesListPorRango
 	  @i_fechaIni datetime
 	, @fecha datetime
 as
 	delete from corteslist
-	;with a as(select @i_fechaIni i, num=1 union all select dateadd(d,1,a.i),num+1 from a where a.i<@fecha)
+	;with a as(select dateadd(d,-1,@i_fechaIni) i, num=1 union all select dateadd(d,1,a.i),num+1 from a where a.i<@fecha)
 	insert into corteslist(c,cortenum)
 	select i,num from a
 	option(maxrecursion 0)
