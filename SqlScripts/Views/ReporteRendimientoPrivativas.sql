107097a45944f009f01b0beab914be7984f9aecf
Eduardo Mancero <emancero@gmail.com>
2026-08-08 01:12:35 -0500

    EMN: MEJ - Incluir ReporteRendimientoPrivativas

---
+++ SqlScripts/Views/ReporteRendimientoPrivativas.sql
@@ -0,0 +1,4 @@
+﻿create view	BVQ_BACKOFFICE.ReporteRendimientoPrivativas as
+	select ppp=sum(saldo/saldoPorTasa*plazo),crc_segmento,saldo=sum(saldo),rpp=crc_tasa/100.0,crc_fecha_cierre
+	from BVQ_BACKOFFICE.RendimientoPrivativas
+	group by crc_segmento,crc_tasa,crc_fecha_cierre
