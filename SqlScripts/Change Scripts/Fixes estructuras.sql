557e9220b855c4c9bac5cb329b1527114e96b28c
Eduardo Mancero <emancero@gmail.com>
2026-08-12 08:47:35 -0500

    EMN: MEJ - En Fixes estructuras comentar algunas secciones que ya se habían aplicado.

--- SqlScripts/Change Scripts/Fixes estructuras.sql
+++ SqlScripts/Change Scripts/Fixes estructuras.sql
@@ -1,15 +1,16 @@
-﻿begin tran
---Cambiar tiv_emisor a 913.
+﻿--Cambiar tiv_emisor a 913.
 --Cacpeco: EMS_ID=85 tiene EMS_ESTADO=22, EMS_ID=913 tiene EMS_ESTADO=21
 --El nombre es idéntico, solo que el nuevo tiene un espacio más
 --Decisión:Publicar
 update bvq_administracion.titulo_valor set tiv_emisor=913 where tiv_id=1e7+85 and tiv_emisor=85
 
 
-
 -----------------------------------------------------------------------------
 --Sin calificación por código SIC errado
---Comprobar que son títulos vencidos hace mucho tiempo (están máximo a sept. de 2024)
+--Vista de errores, no recuerdo cómo se obtuvo, tal vez la calificación estaba vacía
+--exec dropifexists '_temp.tivCodigoTituloSicErr'
+--go
+--Comprobar que son títulos vencidos hace mucho tiempo (vencen máximo a sept. de 2024)
 --son papeles comerciales que no tienen calificación del emisor
 select distinct tiv_fecha_vencimiento,tiv_tipo_valor,eca.eca_id,ems.ems_id,ems_codigo,tiv.tiv_id,ems.ems_nombre--,enc.*
 from
@@ -31,38 +32,12 @@ where ems_codigo in ('car','cea','lfb','spd','nvc','srg','sma','dth','cpb','crs'
 --select tiv_codigo_titulo_sic='02'+right(inscripcion_cpmv,5)
 --from _temp.TempEstructuraIsspolView t join bvq_administracion.titulo_valor tiv on TIV_NUMERO_RMV=inscripcion_cpmv
 --where errores<>'' and tiv_codigo_titulo_sic='0206258'
-select definition from sys.objects o join sys.sql_modules m on o.object_id=m.object_id
-where definition like '%tiv_codigo_titulo_sic%'
-and modify_date>='20221101'
-order by modify_date desc
 
 
 
 
 
-exec dropifexists '_temp.tivCodigoTituloSicErr'
-go
-create view _temp.tivCodigoTituloSicErr as
-	select inscripcion_cpmv,ems_nombre,fecha_vencimiento from
-	(values ('','INMOBILIARIA MONTECRISTI','01-01-1900'),
-	('2012.2.02.01070','DOLMEN S.A.','01-01-1900'),
-	('2013.1.02.01170','ENERGY & PALMA ENERGY PALMA S.A.','01-01-1900'),
-	('2022.G.02.003427','NOVACREDIT S.A.','17-05-2024'),
-	('2022.G.02.003445','FABRICA DE DILUYENTES Y ADHESIVOS DISTHER C. LTDA. DISTHER','07-06-2024'),
-	('2022.G.02.003448','STARCARGO CÍA. LTDA.','25-05-2024'),
-	('2022.G.02.003613','SALCEDO MOTORS S.A. SALMOTORSA','12-04-2024'),
-	('2022.Q.02.003473','SUPERDEPORTE S.A.','21-06-2024'),
-	('2022.Q.02.003561','SUPERDEPORTE S.A.','12-04-2024'),
-	('2022.Q.02.003561','SUPERDEPORTE S.A.','28-02-2024'),
-	('2022.Q.02.003590','CORPORACION ECUATORIANA DE ALIMENTOS Y BEBIDAS CORPABE S.A.','26-03-2024'),
-	('2023.G.02.003643','CORPETROLSA S.A.','19-06-2024'),
-	('2023.G.02.003668','CARTIMEX S.A.','12-04-2024'),
-	('2023.G.02.003685','LA FABRIL S.A','21-09-2024'),
-	('2023.G.02.003764','FABRICA DE DILUYENTES Y ADHESIVOS DISTHER C. LTDA. DISTHER','20-09-2024'),
-	('2023.Q.02.003737','CORPORACIÓN ECUATORIANA DE ALUMINIO S.A. CEDAL','23-09-2024'),
-	('2023.Q.02.003866','PHARMABRAND S.A.','21-11-2024'))
-	v(inscripcion_cpmv,ems_nombre,fecha_vencimiento)
-go
+
 
 
 --Corrige tiv_codigo_titulo_sic según tiv_numero_rmv que está correcto
@@ -137,7 +112,10 @@ where asi_emi_abremi='MDF' and asi_tit_abrtit='BON' and aru_opc_anoope between '
 group by aru_opc_fchval with rollup
 order by opc.aru_opc_fchval
 */
+--EMN: 11-ago-2026 ya no aplica pues ya fue arreglado en producción, se comenta
 --select bolsa_valores,* from _temp.TempEstructuraIsspolViewG2 e where fecha_transaccion='20231231' and bolsa_valores is null
+--select fon_procedencia,numero_liquidacion,e.fecha_compra,*
+/*
 update fon set fon_procedencia='N'
 --output deleted.fon_id,deleted.fon_procedencia into _temp.bakBolsa20260219
 from bvq_backoffice.estructuraisspolview e--_temp.TempEstructuraIsspolViewG2 e
@@ -147,20 +125,25 @@ bvq_backoffice.fondo fon on fon.fon_id=e.fon_id
 --join bvq_administracion.casa_valores cva on g.casval=cva_codigo_sb
 where-- errores not like 'Sin calificación y no es bono.' and errores<>'' and
 numero_liquidacion is null and e.fecha_compra<='20140930' and fon_procedencia is null
-and left(fon_numeracion,3)='MDF' and oper=0
+and left(fon_numeracion,3)='MDF' and oper=0*/
 --Fin Bolsa de valores (procedencia) -------------------------------------------------
 
 
 
-
 --El Fideicomiso Santa Cruz, es extrabursátil
+--EMN: 11-ago-2026 Ya no aplica fue corregido en producción, se comenta
+/*
 update fon set fon_procedencia='N'
+--select fon_procedencia,*
 from bvq_backoffice.fondo fon
 --select tpo_comision_bolsa,* from bvq_backoffice.titulos_portafolio
-where fon_id=475
+where fon_id=475 and isnull(fon_procedencia,'')<>'N'
+*/
 
---El Bono del acta '02-2022, en las estructuras de ejemplo se declara la bolsa como 'N'
+--Bono del acta '02-2022 -------------------------------------------------
+--En las estructuras de ejemplo se declara la bolsa como 'N'
 --no la encontré en las liquidaciones de bolsa. Por otra parte el interés transcurrido está en la comisión de bolsa
+--EMN: 11-ago-2026 Ya no aplica fue corregido en producción, se comenta
 /*
 select *
 from BVQ_BACKOFFICE.operaciones_cerradas oc
@@ -172,6 +155,7 @@ where aru_opc_fchemitit='20220504'--val='20220512'
 group by aru_opc_fchval with rollup
 order by opc.aru_opc_fchval
 */
+/*
 update fon set fon_procedencia='N'
 --select fon_procedencia,*
 --select --tpo_acta,
@@ -180,6 +164,10 @@ update fon set fon_procedencia='N'
 from bvq_backoffice.fondo fon
 join bvq_backoffice.titulos_portafolio tpo on tpo.fon_id=fon.fon_id
 where fon.fon_id=472 and tpo_acta='02-2022'
+and isnull(fon_procedencia,'')<>'N'
+*/
+--Fin Bono del acta '02-2022 ------------------------------------------------
+
 
 /*
 Nombre del emisor	Tipo de instrumento	Id de inversión	Tipo de identificación del emisor	Identificación del emisor	Código identificador del instrumento	Tipo de instrumento	Identificación del instrumento	Bolsa de valores que se negocia	Fecha de emisión	Fecha de compra	Fecha de vencimiento	Tipo tasa	Base para tasa interés	Tasa nominal	Valor nominal	Precio de compra	Valor en efectivo/libros	Plazo inicial	Período de amortización	Periodicidad de pago de cupón	Nro. de documento de aprobación de la inversión	Nro. de resolución / decreto	Nro. de inscripción CPMV	Casa de valores en la que se negocia	Tipo de identificación del custodio	Identificación del Custodio de valores	Calificación de riesgo de la emisión	Calificadora de riesgo de la emisión 	Fecha ultima calificación de riesgos	Precio de mercado	Fecha  precio de mercado	No. Acciones/Unidades de participación	Valor de acción/Unidades de participación 	Fondos de inversión	Errores
@@ -190,8 +178,10 @@ Nombre del emisor	Tipo de instrumento	Id de inversión	Tipo de identificación d
 RETRATOREC S.A.	Acciones	538	R	0992212640001  	07	20	RTT			31/03/2017					21343	0	74700,5	0	RV	RV	SN	SC.IMV.DJMV.DAYR.G.12.0001473	2012.2.02.01033				22	9	14/02/2012	0	31/12/2023	21343	0		
 */
 
---son acciones que aparentemente son aportes directos al portafolio, las de rtt y eyp son dividendos en acciones
+--Acciones que aparentemente son aportes directos al portafolio
+--, las de rtt y eyp son dividendos en acciones
 --no las encontré en el slc ni en el ejemplo proporcionado
+--EMN: 11-ago-2026 Ya no aplica fue corregido en producción, se comenta
 /*
 select a06,* from _temp.g3sh where idemi in ('0990319723001','1391738986001','0992212640001')
 select asi_emi_abremi,aru_opc_numacc*aru_opc_valnomacc,opc.*--asi_emi_abremi,aru_opc_valnomacc,*
@@ -203,6 +193,7 @@ join isspolmay2025.dbo.asi_titulo tit on asi_tit_codtit=aru_opc_codtit
 where asi_emi_abremi in ('dol','rtt','eyp')
 */
 --select * 
+/*
 update fon set fon_procedencia='N'
 --select fon_procedencia,*
 from bvq_backoffice.fondo fon
@@ -211,11 +202,15 @@ join (values ('DOL-','20151224'),('RTT-','20170331'),('EYP-','20230331')) v(abr,
 on fon.fon_numeracion like abr+'%' and tpo_fecha_ingreso=fcompra
 where fon_procedencia is null
 and fon.fon_id in (175,205,538)--472 and tpo_acta='02-2022'
-
+and isnull(fon.fon_procedencia,'')<>'N'
 --select fon_cva_id from bvq_backoffice.fondo where fon_id in (135,172,198,199,200,236,526,505,533)
+*/
+--Fin acciones que son aparentemente aportes directos al portafolio
 
 --Operaciones con bolsa de gye pero sin casa de valores --------------------------------------------------------
 --No sé porqué está el número de liquidación del slc, eso indicaría que no está bien el número de liquidación de Gye
+--EMN: 11-ago-2026 Ya no aplica fue corregido en producción, se comenta
+/*
 if object_id('_temp.bakFonCva20260220') is null
 begin
 	create table _temp.bakFonCva20260220(fon_id int)
@@ -246,6 +241,7 @@ join isspolmay2025.dbo.aru_comven cve on aru_opc_numope=aru_cve_numope and aru_o
 join isspolmay2025.dbo.asi_casval csv on asi_csv_codcasval=aru_cve_codcasval
 join bvq_administracion.casa_valores cva on cva_siglas=asi_csv_abrcasval collate modern_spanish_ci_ai
 where fon_procedencia='G'
+*/
 --Fin operaciones con bolsa de gye pero sin casa de valores --------------------------------------------------------
 
 
@@ -277,27 +273,31 @@ order by opc.aru_opc_fchval
 
 /*
 --update fon set FON_CVA_ID=38
-
 select distinct fon.fon_id,fon_cva_id
-from _temp.TempEstructuraIsspolView e
+from sicavtestbatch._temp.TempEstructuraIsspolView e
 join /*[192.168.2.114].*/sicav.bvq_backoffice.fondo fon on fon.fon_id=e.fon_id
-join _temp.g3sh g on fon_vector_reportado<>'' and g.short=FON_VECTOR_REPORTADO
+join sicavtestbatch._temp.g3sh g on fon_vector_reportado<>'' and g.short=FON_VECTOR_REPORTADO
 --join bvq_administracion.casa_valores cva on g.casval=cva_codigo_sb
 where-- errores not like 'Sin calificación y no es bono.' and errores<>'' and
-numero_liquidacion is null and e.fecha_compra<='20140930'
+numero_liquidacion is null and e.fecha_compra<='20140930' and isnull(fon.fon_cva_id,-1)<>38
+*/
 
---ya está aplicado
+--Obtener código de casa de valores de una aplicación anterior
+--EMN: 11-ago-2026 ya no aplica, ya fue solucionado anteriormente.
+--Se comenta
+/*
 update a set cva_codigo_sb=b.cva_codigo_sb
 --
 --select distinct a.cva_codigo_sb,b.cva_codigo_sb
 from bvq_administracion.casa_valores a
 join --[192.168.2.114].
-sicavtestbatch.bvq_administracion.casa_valores b on a.cva_id=b.cva_id
+sicav.bvq_administracion.casa_valores b on a.cva_id=b.cva_id
 */
 
 --Procedencia y números de liquidación de bonos antiguos---------------------------------------------
---Update: 11-ago-2026 Ya no hace nada porque los números de liquidación ya fueron llenados ne producción
---Se deshabilita
+--Update: 11-ago-2026 Ya no hace nada porque los números de liquidación ya fueron llenados en producción
+--Se comenta
+/*
 ;with a as(
 	--group by aru_opc_fchval,aru_opc_numope --with rollup--,aru_opc_numope --with rollup
 	--order by opc.aru_opc_fchval--,aru_opc_numope
@@ -336,13 +336,14 @@ sicavtestbatch.bvq_administracion.casa_valores b on a.cva_id=b.cva_id
 	join isspolmay2025.dbo.asi_titulo tit on asi_tit_codtit=aru_opc_codtit
 	where asi_emi_abremi='MDF' and asi_tit_abrtit='BON' and aru_opc_anoope between '2013' and '2022'-- between '20130101' and '20230101'--='20140612' --order by opc.aru_opc_valnom,cve.aru_cve_estcomven
 )
-update fon set FON_NUMERO_LIQUIDACION=iif(pr='Q',aru_opc_numope,null),FON_NUMLIQ_TEMP=iif(pr='G',aru_opc_numope,null),FON_PROCEDENCIA=pr-- *
---select aru_opc_numope,pr,fon.*
+--update fon set FON_NUMERO_LIQUIDACION=iif(pr='Q',aru_opc_numope,null),FON_NUMLIQ_TEMP=iif(pr='G',aru_opc_numope,null),FON_PROCEDENCIA=pr-- *
+select aru_opc_numope,pr,fon.*
 from a left join b
 on b.aru_opc_FchVal=a.htp_fecha_operacion and b.aru_opc_valnom=a.montooper and b.aru_opc_fchventit=a.tiv_fecha_vencimiento
 and a.r=b.r
 join bvq_backoffice.fondo fon on fon.fon_id=a.fon_id
 where fon_numero_liquidacion is null and fon_numliq_temp is null and fon_procedencia='N'
+*/
 --order by a.fon_id--3,4,5
 --Fin procedencia y números de liquidación de bonos antiguos---------------------------------------------
 
@@ -355,12 +356,13 @@ from bvq_administracion.variables_balance vba where ems_id=59 and vba_fecha_hast
 --compra_htp_id
 --EMN: 4-abr-2026 primera compra
 --declare @v_tpo_id int = (select htp_tpo_id from bvq_backoffice.historico_titulos_portafolio where htp_id=@v_htp_id)
+/*
 update htp set compra_htp_id=primeraCompra.htp_id
 --select *
 from bvq_backoffice.historico_titulos_portafolio htp
 join bvq_backoffice.SecuenciaCompra primeraCompra
 	on primeraCompra.htp_tpo_id=htp.htp_tpo_id and primeraCompra.sec=1
-
---where isnull(compra_htp_id,-1)<>isnull(primeraCompra.htp_id,-1)
+where isnull(compra_htp_id,-1)<>isnull(primeraCompra.htp_id,-1)
 --where htp.htp_tpo_id=@v_tpo_id
-rollback tran
+*/
+
