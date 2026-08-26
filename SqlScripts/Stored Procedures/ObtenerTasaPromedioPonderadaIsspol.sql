5a06a8004a4d77ce95430ea5626a03dfe20f0d3f
David Betancourt <ddavebet@gmail.com>
2026-08-26 12:18:49 -0500

    DBN: FIX - Corregir cálculo de tpp en ObtenerTasaPromedioPonderadaIsspol

--- SqlScripts/Stored Procedures/ObtenerTasaPromedioPonderadaIsspol.sql
+++ SqlScripts/Stored Procedures/ObtenerTasaPromedioPonderadaIsspol.sql
@@ -24,7 +24,12 @@ BEGIN
               pc.htp_numeracion
 
     SELECT 
-        [SECTOR_DETALLADO] collate modern_spanish_ci_ai AS titulo,
+        SECTOR_DETALLADO=CASE [sector_general]
+			WHEN 'SEC_PRI_FIN' THEN sector_detallado--'PRIVADO FINANCIERO Y ECONOMÍA POPULAR SOLIDARIA'
+			WHEN 'SEC_PRI_NFIN' THEN 'PRIVADO NO FINANCIERO'
+			WHEN 'SEC_PUB_FIN' THEN 'PUBLICO'
+			WHEN 'SEC_PUB_NFIN' THEN 'PUBLICO'
+		END,
         SUM(sal) AS monto,
         SUM(CASE
                 WHEN pc.[tvl_codigo] IN ('FAC','PCO','OBL','OCA','VCC')
@@ -33,10 +38,17 @@ BEGIN
                     THEN [htp_rendimiento]
                 ELSE [tiv_tasa_interes]
             END / 100.0 * sal) / NULLIF(SUM(sal), 0) AS tpp
-    FROM   bvq_backoffice.portafoliocorte pc
+    FROM   bvq_backoffice.portafoliocorteprcint pc
     WHERE  ISNULL(ipr_es_cxc, 0) = 0
-           AND sal > 0
-    GROUP BY SECTOR_DETALLADO collate modern_spanish_ci_ai
+           AND sal > 0 
+           and tiv_tipo_renta = 153
+    GROUP BY 
+        CASE [sector_general]
+			WHEN 'SEC_PRI_FIN' THEN sector_detallado--'PRIVADO FINANCIERO Y ECONOMÍA POPULAR SOLIDARIA'
+			WHEN 'SEC_PRI_NFIN' THEN 'PRIVADO NO FINANCIERO'
+			WHEN 'SEC_PUB_FIN' THEN 'PUBLICO'
+			WHEN 'SEC_PUB_NFIN' THEN 'PUBLICO'
+		END
 
     SELECT 'Tasa Promedio Ponderada General' AS titulo,
            SUM(sal) AS monto,
