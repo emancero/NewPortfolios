f2138e8f3390a912ae55ad9d5b3f7663f1a2597d
Eduardo Mancero <emancero@gmail.com>
2026-09-01 15:35:58 -0500

    EMN: ISS - Aumentar ObtenerCapitalSuscritoEmisor a git.

---
+++ SqlScripts/Stored Procedures/ObtenerCapitalSuscritoEmisor.sql
@@ -0,0 +1,14 @@
+﻿CREATE procedure BVQ_ADMINISTRACION.ObtenerCapitalSuscritoEmisor
+	@i_ems_id int,
+	@i_lga_id INT = NULL
+as
+begin
+
+	select CCA_ID, 
+		EMI_ID, 
+		CCA_SUSCRITO, 
+		CCA_FECHA_ACTUALIZACION 
+		from BVQ_ADMINISTRACION.COMPOSICION_CAPITAL 
+		where EMI_ID = @i_ems_id
+
+end
