1cde825c851b1538884d5c4a9b5caab297f6a180
David Betancourt <ddavebet@gmail.com>
2026-09-24 17:24:26 -0500

    DBN: ISS - Guardar proyecciones manuales en Reporte de Disponibilidad

---
+++ SqlScripts/Stored Procedures/InsertarProyeccionDispCabISSPOL.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [BVQ_BACKOFFICE].[InsertarProyeccionDispCabISSPOL]
+    @i_nombre        VARCHAR(200),
+    @i_fecha_corte   DATE,
+    @i_fecha_fin     DATE,
+    @i_lga_id int
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    INSERT INTO [BVQ_BACKOFFICE].[ISSPOL_PROYECCION_DISP_CAB]
+        (PDC_NOMBRE, PDC_FECHA_CORTE, PDC_FECHA_FIN)
+    VALUES
+        (@i_nombre, @i_fecha_corte, @i_fecha_fin)
+
+    SELECT CAST(SCOPE_IDENTITY() AS INT) AS PDC_ID
+END
---
+++ SqlScripts/Stored Procedures/InsertarProyeccionDispDetISSPOL.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [BVQ_BACKOFFICE].[InsertarProyeccionDispDetISSPOL]
+    @i_pdc_id             INT,
+    @i_fecha_vencimiento  DATE,
+    @i_portafolio         VARCHAR(200) = NULL,
+    @i_subtipo            VARCHAR(200) = NULL,
+    @i_tipo_papel         VARCHAR(200) = NULL,
+    @i_ie                 VARCHAR(20)  = NULL,
+    @i_monto              MONEY,
+    @i_lga_id int
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    INSERT INTO [BVQ_BACKOFFICE].[ISSPOL_PROYECCION_DISP_DET]
+        (PDD_PDC_ID, PDD_FECHA_VENCIMIENTO, PDD_PORTAFOLIO, PDD_SUBTIPO,
+         PDD_TIPO_PAPEL, PDD_IE, PDD_MONTO)
+    VALUES
+        (@i_pdc_id, @i_fecha_vencimiento, @i_portafolio, @i_subtipo,
+         @i_tipo_papel, @i_ie, @i_monto)
+END
